a25fc9210b52feeb42116cdb713e018570b3de46
carlospasichana <pasichanacarlos3@gmail.com>
2024-06-04 22:01:02 -0500

    fundamentos de programacion con Js

---
+++ javascript/conceptosBasicos/scripts/index.js
@@ -0,0 +1,55 @@
+<!--- comentario en html --->
+/* comentario en css - comentario en bloque de js */
+// comentario de linea en js
+
+
+console.log("Hola mundo - estoy vinculado con mi html");
+
+// 1. variables en JS
+
+/*Definición: espacio de memoria --> cajita que me guarda 
+información en forma de datos --> diferentes tipos numericos ( 2,  2.3, 4.34534533)
+boleanos, arreglos, texto (strings), objetos*/
+
+// Definimos para usarlo constantemente
+
+/* Variables en JS 
+1. var: Nos guarda datos que pueden cambiar en el tiempo ( en desuso) ya no se usa
+
+la que usaremos
+2. let: Nos guarda datos que pueden cambiar en el tiempo
+
+3. const: Nos guarda datos constantes es decir no cambian en el tiempo
+*/
+
+//2. declaracion de variables 
+
+/*
+1. Definir que tipo de variable (let o const)
+2. Darle nombre a la variable 
+3. decirle que va a guardar
+*/
+
+let nombreVariable = "nombre"
+
+
+// numéricos
+
+let numero = 5;
+const pi = 3.14;
+
+// Texto o strings
+
+let texto = "hola"
+const nombre = "carlos"
+
+//boleanos --> verdadero o falso 
+
+let esVerdadero = true;
+const esFalso = false;
+
+//arreglos me permiten guardar mas de un dato por buenas practicas 
+// se reconocen por el uso de corchetes y los datos se separan por comas
+let arregloNumero = [5, 3, 7, 9]
+const arregloNombre = ["luisa", "daniel"];
+
